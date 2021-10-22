b4cc90d633618f04dfd9b2bd33518bd378de0816
artem.malieiev <artem.malieiev@exclaimer.com>
2021-10-22 18:12:20 +0300

    added gapi

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -3,14 +3,38 @@ import { useParent } from "../core/index.js";
 export function LoginPage({ onLogin }) {
     useParent((el) => {
         el.querySelector("button").addEventListener("click", () => {
-            localStorage.setItem("token", "secret token");
-            location.hash = "todos";
-            onLogin();
+            gapi.load("auth2", () => {
+                gapi.auth2
+                    .init({
+                        client_id:
+                            "277836425026-d32flpp7m11qlkkaknfrn4485dqmkfd4.apps.googleusercontent.com",
+                    })
+                    .then((googleAuth) => {
+                        const authResponse = googleAuth.currentUser
+                            .get()
+                            .getAuthResponse();
+                        const token = authResponse.id_token;
+
+                        console.log(token);
+
+                        gapi.auth2
+                            .getAuthInstance()
+                            .signIn()
+                            .then((user) => {
+                                console.log(user.getAuthResponse());
+                                localStorage.setItem(
+                                    "token",
+                                    user.getAuthResponse().id_token
+                                );
+                                location.hash = "todos";
+                                onLogin();
+                            });
+                    });
+            });
         });
     });
 
     return `
-        <input type="text">
-        <button>login</button>
+        <button>Login with Google</button>
     `;
 }
